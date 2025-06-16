b93d86ff08de12b64f7392db3c4a1b5133f4bec8
bryzgalovdm <bryzgalovdm@gmail.com>
2025-06-16 21:09:24 +0200

    [test] change nodes number

--- animation_nn.js
+++ animation_nn.js
@@ -11,7 +11,7 @@ resizeCanvas();
 
 const nodes = [];
 const connections = [];
-const nodeCount = 200;
+const nodeCount = 250;
 const nodeRadius = 2;
 const connectionDistance = 150;
 const signalSpeed = 0.05;
